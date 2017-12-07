9f50ee0ea653f21264c01c4a138f2d642ce8b109
Vahidd <vahidd.com@gmail.com>
2017-12-07 10:39:56 +0330

    Add waypoint. Then add new releases component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,9 @@ import Sidebar from 'Components/sidebar/Sidebar';
 import { withRouter } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
 
+import generalStyles from 'Styles/general';
 import * as RouteConstant from 'Constants/RouteConstants';
-import NewReleases from 'Components/NewReleases';
+import NewReleases from 'Containers/NewReleasesContainer';
 import TopCharts from 'Components/TopCharts';
 import Logout from 'Components/Logout';
 
@@ -12,12 +13,14 @@ class App extends React.Component {
   render () {
     return <div>
       <Sidebar/>
-      <Switch>
-        <Route path={RouteConstant.HOME} exact component={NewReleases}/>
-        <Route path={RouteConstant.NEW_RELEASES} component={NewReleases}/>
-        <Route path={RouteConstant.TOP_CHARTS} component={TopCharts}/>
-        <Route path={RouteConstant.LOGOUT} component={Logout}/>
-      </Switch>
+      <div className={generalStyles.content}>
+        <Switch>
+          <Route path={RouteConstant.HOME} exact component={NewReleases}/>
+          <Route path={RouteConstant.NEW_RELEASES} component={NewReleases}/>
+          <Route path={RouteConstant.TOP_CHARTS} component={TopCharts}/>
+          <Route path={RouteConstant.LOGOUT} component={Logout}/>
+        </Switch>
+      </div>
     </div>;
   }
 }
--- src/components/NewReleases.jsx
+++ src/components/NewReleases.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import Waypoint from 'react-waypoint';
+
+import CommonStyles from 'Styles/common.scss';
+import AlbumItem from 'Components/common/AlbumItem';
 
 export default class NewReleases extends React.Component {
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      page          : 1,
+      infiniteScroll: false
+    };
+    this.onWaypointEnter = this.onWaypointEnter.bind(this);
+  }
+
+  componentDidMount () {
+    this.props.fetchNewReleases(40, (this.state.page - 1) * 40);
+    this.infiniteScrollTimeout = setTimeout(() => {
+      this.setState({infiniteScroll: true});
+    }, 1000);
+  }
+
+  componentWillUnmount () {
+    clearTimeout(this.infiniteScrollTimeout);
+  }
+
+  onWaypointEnter () {
+    if (this.props.newReleases.isFetching) {
+      return;
+    }
+    this.setState({page: this.state.page + 1});
+    this.props.fetchNewReleases(40, (this.state.page - 1) * 40);
+  }
+
   render () {
+    let {data} = this.props.newReleases;
+    if (!data) {
+      return null;
+    }
     return <div>
-      New releases :)
+      <div className={CommonStyles.title}>
+        <h1>New Releases</h1>
+      </div>
+      <Row gutter={40}>
+        {data.map((album, index) => {
+          return <Col key={index} span={6}>
+            <AlbumItem album={album}/>
+          </Col>;
+        })}
+      </Row>
+      {this.state.infiniteScroll && <Waypoint
+        bottomOffset="-500px"
+        onEnter={this.onWaypointEnter}
+      />}
     </div>;
   }
 }
