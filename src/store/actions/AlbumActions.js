f431b72ea0fb0a5820ba92f2b9b7498f1cc70881
Vahidd <vahidd.com@gmail.com>
2017-12-22 15:22:45 +0330

    Add fetch album artists option to fetchAlbum action

--- src/store/actions/AlbumActions.js
+++ src/store/actions/AlbumActions.js
@@ -1,5 +1,8 @@
 import * as ActionsConstants from 'Constants/ActionConstants';
 import { axiosInstance } from 'Services/UtilsService';
+import { each } from 'lodash';
+
+import { fetchArtist } from 'Actions/ArtistActions';
 
 function fetchAlbumRequest () {
   return {
@@ -15,11 +18,15 @@ function fetchAlbumResponse (albumId, response) {
   };
 }
 
-export function fetchAlbum (albumId) {
+export function fetchAlbum (albumId, fetchArtists = false) {
   return (dispatch) => {
     dispatch(fetchAlbumRequest());
     return axiosInstance().get(CONFIGS.API_URL + '/albums/' + albumId)
       .then((res) => {
+        if (fetchArtists)
+          each(res.data.artists, (artist) => {
+            dispatch(fetchArtist(artist.id));
+          });
         dispatch(fetchAlbumResponse(albumId, res.data));
       });
   };
